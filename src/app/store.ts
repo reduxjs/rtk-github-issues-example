262fbe422c517388de26e6a53c1586b171c5b061
Mark Erikson <mark@isquaredsoftware.com>
2019-08-21 16:08:03 -0400

    Add store and root reducer with reducer HMR

---
+++ src/app/store.ts
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import rootReducer from './rootReducer'
+
+const store = configureStore({
+  reducer: rootReducer
+})
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./rootReducer', () => {
+    const newRootReducer = require('./rootReducer').default
+    store.replaceReducer(newRootReducer)
+  })
+}
+
+export type AppDispatch = typeof store.dispatch
+
+export default store

26054ea8be1a44cac75fd55f497ce20e264de2b0
Mark Erikson <mark@isquaredsoftware.com>
2019-08-21 16:08:03 -0400

    Add store and root reducer with reducer HMR

---
+++ src/app/rootReducer.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux-starter-kit'
+
+const rootReducer = combineReducers({})
+
+export type RootState = ReturnType<typeof rootReducer>
+
+export default rootReducer

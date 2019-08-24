5fda8e1c82b37b1330dd7707fd0b59bf0791fb83
Mark Erikson <mark@isquaredsoftware.com>
2019-08-24 11:48:00 -0600

    Add AppThunk type

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, Action } from '@reduxjs/toolkit'
+import { ThunkAction } from 'redux-thunk'
 
-import rootReducer from './rootReducer'
+import rootReducer, { RootState } from './rootReducer'
 
 const store = configureStore({
   reducer: rootReducer
@@ -14,5 +15,7 @@ if (process.env.NODE_ENV === 'development' && module.hot) {
 }
 
 export type AppDispatch = typeof store.dispatch
+
+export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>
 
 export default store

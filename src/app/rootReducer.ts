da9291bf428a96c3f2e8862f42e3be08461d514c
Mark Erikson <mark@isquaredsoftware.com>
2019-08-25 19:34:06 -0600

    Add a slice for storing repo details

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux-starter-kit'
 
 import issuesDisplayReducer from 'features/issuesDisplay/issuesDisplaySlice'
+import repoDetailsReducer from 'features/repoSearch/repoDetailsSlice'
 
 const rootReducer = combineReducers({
-  issuesDisplay: issuesDisplayReducer
+  issuesDisplay: issuesDisplayReducer,
+  repoDetails: repoDetailsReducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>

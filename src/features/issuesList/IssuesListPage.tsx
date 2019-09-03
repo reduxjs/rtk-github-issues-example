8dbdc0726ccecf354a01351786196648c752c0a6
Mark Erikson <mark@isquaredsoftware.com>
2019-09-02 23:16:16 -0400

    Update IssuesListPage to fetch issues data via Redux

--- src/features/issuesList/IssuesListPage.tsx
+++ src/features/issuesList/IssuesListPage.tsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-
-import { getIssues, IssuesResult } from 'api/githubAPI'
 
 import { fetchIssuesCount } from 'features/repoSearch/repoDetailsSlice'
 import { RootState } from 'app/rootReducer'
@@ -9,6 +7,7 @@ import { RootState } from 'app/rootReducer'
 import { IssuesPageHeader } from './IssuesPageHeader'
 import { IssuesList } from './IssuesList'
 import { IssuePagination, OnPageChangeCallback } from './IssuePagination'
+import { fetchIssues } from './issuesSlice'
 
 interface ILProps {
   org: string
@@ -27,43 +26,25 @@ export const IssuesListPage = ({
 }: ILProps) => {
   const dispatch = useDispatch()
 
-  const [issuesResult, setIssues] = useState<IssuesResult>({
-    pageLinks: null,
-    pageCount: 1,
-    issues: []
-  })
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [issuesError, setIssuesError] = useState<Error | null>(null)
+  const {
+    currentPageIssues,
+    isLoading,
+    error: issuesError,
+    issuesByNumber,
+    pageCount
+  } = useSelector((state: RootState) => state.issues)
+
   const openIssueCount = useSelector(
     (state: RootState) => state.repoDetails.openIssuesCount
   )
 
-  const { issues, pageCount } = issuesResult
+  const issues = currentPageIssues.map(
+    issueNumber => issuesByNumber[issueNumber]
+  )
 
   useEffect(() => {
-    async function fetchEverything() {
-      async function fetchIssues() {
-        const issuesResult = await getIssues(org, repo, page)
-        setIssues(issuesResult)
-      }
-
-      try {
-        await Promise.all([
-          fetchIssues(),
-          dispatch(fetchIssuesCount(org, repo))
-        ])
-        setIssuesError(null)
-      } catch (err) {
-        console.error(err)
-        setIssuesError(err)
-      } finally {
-        setIsLoading(false)
-      }
-    }
-
-    setIsLoading(true)
-
-    fetchEverything()
+    dispatch(fetchIssues(org, repo, page))
+    dispatch(fetchIssuesCount(org, repo))
   }, [org, repo, page, dispatch])
 
   if (issuesError) {

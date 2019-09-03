9d1246a4d89f21da1f0e5377f040bc766e1fc0fd
Mark Erikson <mark@isquaredsoftware.com>
2019-09-03 00:17:54 -0400

    Update IssueDetailsPage to fetch comments via Redux

--- src/features/issueDetails/IssueDetailsPage.tsx
+++ src/features/issueDetails/IssueDetailsPage.tsx
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch, shallowEqual } from 'react-redux'
 import ReactMarkdown from 'react-markdown'
 import classnames from 'classnames'
 
 import { insertMentionLinks } from 'utils/stringUtils'
-import { getComments, Comment } from 'api/githubAPI'
 import { IssueLabels } from 'components/IssueLabels'
 import { RootState } from 'app/rootReducer'
 import { fetchIssue } from 'features/issuesList/issuesSlice'
 
 import { IssueMeta } from './IssueMeta'
 import { IssueComments } from './IssueComments'
+import { fetchComments } from './commentsSlice'
 
 import styles from './IssueDetailsPage.module.css'
 import './IssueDetailsPage.css'
@@ -28,13 +28,21 @@ export const IssueDetailsPage = ({
   issueId,
   showIssuesList
 }: IDProps) => {
-  const [comments, setComments] = useState<Comment[]>([])
-  const [commentsError] = useState<Error | null>(null)
-
   const dispatch = useDispatch()
 
   const issue = useSelector(
     (state: RootState) => state.issues.issuesByNumber[issueId]
+  )
+
+  const { commentsLoading, commentsError, comments } = useSelector(
+    (state: RootState) => {
+      return {
+        commentsLoading: state.comments.loading,
+        commentsError: state.comments.error,
+        comments: state.comments.commentsByIssue[issueId]
+      }
+    },
+    shallowEqual
   )
 
   useEffect(() => {
@@ -47,15 +55,10 @@ export const IssueDetailsPage = ({
   }, [org, repo, issueId, issue, dispatch])
 
   useEffect(() => {
-    async function fetchComments() {
-      if (issue) {
-        const comments = await getComments(issue.comments_url)
-        setComments(comments)
-      }
+    if (issue) {
+      dispatch(fetchComments(issue))
     }
-
-    fetchComments()
-  }, [issue])
+  }, [issue, dispatch])
 
   let content
 
@@ -65,16 +68,6 @@ export const IssueDetailsPage = ({
     </button>
   )
 
-  if (commentsError) {
-    return (
-      <div className="issue-detail--error">
-        {backToIssueListButton}
-        <h1>There was a problem loading issue #{issueId}</h1>
-        <p>{commentsError.toString()}</p>
-      </div>
-    )
-  }
-
   if (issue === null) {
     content = (
       <div className="issue-detail--loading">
@@ -83,7 +76,24 @@ export const IssueDetailsPage = ({
       </div>
     )
   } else {
-    let renderedComments = <IssueComments issue={issue} comments={comments} />
+    let renderedComments
+
+    if (comments) {
+      renderedComments = <IssueComments issue={issue} comments={comments} />
+    } else if (commentsLoading) {
+      renderedComments = (
+        <div className="issue-detail--loading">
+          <p>Loading comments...</p>
+        </div>
+      )
+    } else if (commentsError) {
+      renderedComments = (
+        <div className="issue-detail--error">
+          <h1>Could not load comments for issue #{issueId}</h1>
+          <p>{commentsError.toString()}</p>
+        </div>
+      )
+    }
 
     content = (
       <div className={classnames('issueDetailsPage', styles.issueDetailsPage)}>

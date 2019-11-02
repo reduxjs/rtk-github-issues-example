4fa1bfffd787639f5f3d0dbab12758ccc851e293
Mark Erikson <mark@isquaredsoftware.com>
2019-11-02 17:28:00 -0400

    Add a slice for tracking issues state

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -2,10 +2,12 @@ import { combineReducers } from '@reduxjs/toolkit'
 
 import issuesDisplayReducer from 'features/issuesDisplay/issuesDisplaySlice'
 import repoDetailsReducer from 'features/repoSearch/repoDetailsSlice'
+import issuesReducer from 'features/issuesList/issuesSlice'
 
 const rootReducer = combineReducers({
   issuesDisplay: issuesDisplayReducer,
-  repoDetails: repoDetailsReducer
+  repoDetails: repoDetailsReducer,
+  issues: issuesReducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
